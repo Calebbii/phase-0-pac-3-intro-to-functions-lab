f0fbc78f96b0d6e56c3a677350bb5b7996f2bc7c
Calebbii <biicaleb316@gmail.com>
2022-08-01 23:34:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    return console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    return console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toUpperCase());
+    return "YES INDEED!"
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase());
+    return "I can't hear you!"
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase());
+    return "I can't hear you!"
+}
+console.log(string.toUpperCase());
+
+string === string.toUpperCase();
